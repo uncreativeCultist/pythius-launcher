b8fb678b37ba5d06e24b75b132679fad04675f95
Dahlia B. <uncreative@cultist.gay>
2026-03-08 07:43:39 -0400

    update build script

--- BuildScripts/compile_windows.bat
+++ BuildScripts/compile_windows.bat
@@ -1,3 +1,3 @@
 @echo off
 cd ../
-pyinstaller --noconfirm --hidden-import dearpygui --windowed --icon "./Resources/pythius.ico"  "./pythius.py"
+pyinstaller --noconfirm --hidden-import dearpygui --hidden-import discord-rich-presence --windowed --icon "./Resources/pythius.ico"  "./pythius.py"
